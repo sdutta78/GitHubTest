cbb9cfb1939a3a338abf4396fd4ecb2fc4149be4
Sushant Dutta <sushantdutta007@yahoo.com>
2020-02-07 14:57:58 +0530

    Added a single line comment

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+//Added a single line comment
 
 namespace GitHubTest
 {
